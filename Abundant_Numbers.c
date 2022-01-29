0a9e9e73d6459698f7276b13f8f2d1eeea014910
Bivesh01 <98143801+Bivesh01@users.noreply.github.com>
2022-01-29 15:34:57 +0530

    #include<stdio.h> int main() {     int n,i,sum=0,stored;     scanf("%d",&n);     stored=n;     for(i=1;i<n;i++)     {         if(n%i==0)         {             sum=sum+i;         }     }
        if(stored<sum)
        {
        printf("True");
        }
        else
        {
        printf("False");
        }
    }

---
+++ Abundant_Numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,sum=0,stored;
+    scanf("%d",&n);
+    stored=n;
+    for(i=1;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            sum=sum+i;
+        }
+    }
+  
+    if(stored<sum)
+    {
+    printf("True");
+    }
+    else
+    {
+    printf("False");
+    }
+}
