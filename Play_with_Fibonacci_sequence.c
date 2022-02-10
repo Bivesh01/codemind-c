263776fc870150a1bd67f135c8bcd3fb6cdf2d7d
bivesh0 <98143801+bivesh0@users.noreply.github.com>
2022-02-10 18:42:57 +0530

    #include<stdio.h> int main() {     int n,i,f=0,f1=0,f2=1;     scanf("%d",&n);
        for(i=1;i<=n;i++)
        {
            printf("%d ",f);
            f1=f2;
            f2=f;
            f=f1+f2;
            
        }
        
    }

---
+++ Play_with_Fibonacci_sequence.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,f=0,f1=0,f2=1;
+    scanf("%d",&n);
+    
+    for(i=1;i<=n;i++)
+    {
+        printf("%d ",f);
+        f1=f2;
+        f2=f;
+        f=f1+f2;
+        
+    }
+    
+}
