3054b633951e1ca1fa8dda1ecc52051fa2700379
bivesh0 <98143801+bivesh0@users.noreply.github.com>
2022-02-16 22:22:56 +0530

    #include<stdio.h> int main() {     int n,i,arr1[i],arr2[i],sum;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr1[i]);         scanf("%d",&arr2[i]);         sum=arr1[i]+arr2[i];         printf("%d ",sum);     } }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr1[i],arr2[i],sum;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr1[i]);
+        scanf("%d",&arr2[i]);
+        sum=arr1[i]+arr2[i];
+        printf("%d
+",sum);
+    }
+}
